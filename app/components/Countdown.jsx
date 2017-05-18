568b936ff1229b2ba08fe5b70f3ca74334dde301
Mike Reed <Mike Reed>
2017-05-18 12:14:37 -0700

    initial commit

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+  //
+  render: function () {
+    return (
+        //
+        <div>
+          <p>Countdown Component</p>
+        </div>
+    );
+  }
+});
+
+module.exports = Countdown;
---
+++ app/components/Nav.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+  //  debugger;
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/countdown" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+              Created By: 
+              <a href="https://www.linkedin.com/in/mikereed111/" target="_blank">Mike Reed</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Nav;
